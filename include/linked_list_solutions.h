db0f6c718bb3207686d14a678eba64e778dd0289
SWAT <peng.wu.hust@hotmail.com>
2023-01-16 19:00:48 +0800

    add linked_list_solutions lib

---
+++ include/linked_list_solutions.h
@@ -0,0 +1,11 @@
+#ifndef LINKED_LIST_SOLUTIONS_H
+#define LINKED_LIST_SOLUTIONS_H
+
+struct LinkNode {
+  int value;
+  LinkNode* next;
+  LinkNode(int x): value(x), next(nullptr) {}
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "array_solutions.h"
+#include "linked_list_solutions.h"
 
 int main() {
   return 0;
